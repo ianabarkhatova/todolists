a8e649d4919bc8845d1a2f88a19b8515fe743d6d
Iana Barkhatova <yanabarhat@gmail.com>
2024-10-17 14:37:29 +0300

    Changed FormLabel text.

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -62,13 +62,13 @@ export const Login = () => {
         <form onSubmit={formik.handleSubmit}>
           <FormControl>
             <FormLabel>
-              <p>
-                To log in get registered
-                <a href={"https://social-network.samuraijs.com/"} target={"_blank"}>
-                  here
-                </a>
-              </p>
-              <p>or use common test account credentials:</p>
+              {/*<p>*/}
+              {/*  To log in get registered*/}
+              {/*  <a href={"https://social-network.samuraijs.com/"} target={"_blank"}>*/}
+              {/*    here*/}
+              {/*  </a>*/}
+              {/*</p>*/}
+              <p>To log in, use test account credentials:</p>
               <p>Email: free@samuraijs.com</p>
               <p>Password: free</p>
             </FormLabel>
